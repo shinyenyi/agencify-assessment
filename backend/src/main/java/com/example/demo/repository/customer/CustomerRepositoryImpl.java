36bb0816560d226706f81ca90f42093dbc66a874
Miguel Shinyenyi <miguelshinyenyi@gmail.com>
2023-01-16 16:47:52 +0300

    add @repository annotation

--- backend/src/main/java/com/example/demo/repository/customer/CustomerRepositoryImpl.java
+++ backend/src/main/java/com/example/demo/repository/customer/CustomerRepositoryImpl.java
@@ -4,22 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Service;
-
+import org.springframework.stereotype.Repository;
 import com.example.demo.domain.customer.Customer;
 
-@Service
+@Repository
 public class CustomerRepositoryImpl implements CustomerRepository {
 
 	@Autowired
 	public JdbcTemplate jdbcTemplate;
 
+	@Override
 	public List<Customer> getAllCustomers() {
 		List<Customer> customers = jdbcTemplate.query("SELECT*FROM customer",
 				(resultSet, rowNum) -> new Customer(resultSet.getInt("id"), resultSet.getString("name"),
 						resultSet.getString("phone")));
 
 		return customers;
+	}
+	
+	public List<String> getAllPhonenumbers() {
+		List<String> phonenumbers = jdbcTemplate.query("SELECT phone FROM customer",
+				(resultSet, rowNum)-> new String(resultSet.getString("phone")));
+		
+		return phonenumbers;
 	}
 
 }
